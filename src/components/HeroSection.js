fa26d088a6d38addb1cedef443d9db4425c77fe7
alan449 <89176065+alan449@users.noreply.github.com>
2023-01-27 21:22:53 +1100

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,7 +5,7 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src="videos/video-1.mp4" autoPlay loop muted />
+      <video src="./luckystar/video-1.mp4" autoPlay loop muted />
       <h1>CUSTOM CLEARANCE</h1>
       <p>export from / import to China</p>
       <p>Get A <span>Free</span> Quote!</p>
